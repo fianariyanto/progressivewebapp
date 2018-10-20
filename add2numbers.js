65a72a3669946cfedda31bfadc1d799fadefaccb
fianariyanto <fian.ariyanto90@gmail.com>
2018-10-20 16:17:45 +0700

    first commit

---
+++ add2numbers.js
@@ -0,0 +1,10 @@
+function kalkulator() {
+let angka = document.querySelectorAll('input');
+let i1= angka[0].value ; //angka pertama
+let i2= angka[1].value ; //angka kedua
+angka[2].value= parseInt(i1) + parseInt(i2) ;
+let pesan=document.getElementById('message');
+pesan.innerHTML="Selesai";
+}
+let tombol = document.querySelector('button');
+tombol.addEventListener('click', kalkulator);
